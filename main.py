17d8d6f26e4641420d0e4efe37426024f3f7edff
waldofx <90760767+waldofx@users.noreply.github.com>
2022-10-19 09:48:39 +0700

    Tugas 2 better image

--- main.py
+++ main.py
@@ -3,23 +3,19 @@ from PIL import Image
 import cv2
 import matplotlib.pyplot as plt
 
-file_name = "mario.jpg"
-
+file_name = "gaugan4.jpg"
 img = Image.open(file_name)
-
-img = img.resize((5, 5))
-
+# img = img.resize((5, 5))
 img = img.convert("RGB")
-
 img.save("output.jpg")
-
 width, height = img.size
 
-for h in range(height):
-    for w in range(width):
-        pixel_value = img.getpixel((w,h))
-        print(pixel_value, end= ")")
-    print()
+# for h in range(height):
+#     for w in range(width):
+#         pixel_value = img.getpixel((w,h))
+#         print(pixel_value, end= ")")
+#     print()
+
 
 # tugas 2 histogram gray
 img = cv2.imread(file_name,0)
@@ -31,7 +27,7 @@ plt.xticks([])
 plt.yticks([])
 
 plt.subplot(1,2,2)
-plt.hist(img.ravel(),256,[0,255])
+plt.hist(img.ravel(),256,[0,256])
 plt.title('histogram')
 
 plt.show()
